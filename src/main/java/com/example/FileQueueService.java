633810cd207e67575667daf173a5cae40e507aac
Ben Boyter <boyter@Bens-MacBook-Pro.local>
2016-03-01 08:27:17 +1100

    File locks are atomic on posix, not sure if Java keeps that? Good enough for canva blog, good enough for me.

--- src/main/java/com/example/FileQueueService.java
+++ src/main/java/com/example/FileQueueService.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.io.File;
+
 public class FileQueueService implements QueueService {
     public void push(QueueMessage message) {
     }
@@ -9,5 +11,15 @@ public class FileQueueService implements QueueService {
     }
 
     public void delete(QueueMessage message) {
+    }
+
+    private void lock(File lock) throws InterruptedException {
+        while (!lock.mkdir()) {
+            Thread.sleep(50);
+        }
+    }
+
+    private void unlock(File lock) {
+        lock.delete();
     }
 }
