b2217238336acbcd8f89999433e567603aab0f0e
Ben Boyter <boyter@Bens-MacBook-Pro.local>
2016-03-03 08:32:37 +1100

    Comments

--- src/main/java/com/example/SqsQueueService.java
+++ src/main/java/com/example/SqsQueueService.java
@@ -8,6 +8,13 @@ import com.example.exceptions.QueueFullException;
 
 import java.util.List;
 
+/**
+ * Implementation of our queue backed by Amazon's SQS Queue
+ * kinda... this most likely will not work as below, but I
+ * don't really feel like spinning everything up to verify
+ * and that's difficult anyway when you have a spotty internet
+ * connection on a train.
+ */
 public class SqsQueueService implements QueueService {
   private final AmazonSQSClient sqsClient;
 
@@ -17,6 +24,13 @@ public class SqsQueueService implements QueueService {
     this.sqsClient = sqsClient;
   }
 
+  /**
+   * Adds message to SQS queue based on the AmazonSQSClient configured
+   * in the constructor.
+   * @param queueName
+   * @param message
+   * @throws QueueFullException
+   */
   @Override
   public void push(String queueName, QueueMessage message) throws QueueFullException {
     sqsClient.sendMessage("queueName", message.getMessage());
@@ -27,6 +41,13 @@ public class SqsQueueService implements QueueService {
     sqsClient.sendMessage(myQueueUrl, message.getMessage());
   }
 
+  /**
+   * Pulls a message from SQS queue based on the AmazonSQSClient configured
+   * in the constructor
+   * @param queueName
+   * @param visibilityTimeout
+   * @return
+   */
   @Override
   public QueueMessage pull(String queueName, long visibilityTimeout) {
     QueueMessage queueMessage = null;
@@ -45,6 +66,12 @@ public class SqsQueueService implements QueueService {
     return queueMessage;
   }
 
+  /**
+   * Deletes a message from SQS queue based on the AmazonSQSClient configured
+   * in the constructor
+   * @param queueName
+   * @param message
+   */
   @Override
   public void delete(String queueName, QueueMessage message) {
     CreateQueueRequest createQueueRequest = new CreateQueueRequest(queueName);
